e5254a4bb0599a5bee44cb5f80ff89c7daa77ed6
segfaultdev <segfaultdev@protonmail.com>
2021-08-06 20:00:12 +0200

    voronoi!

--- scripts/index.js
+++ scripts/index.js
@@ -1,123 +1,66 @@
-let dina, dina_bold;
-
-let grid = [];
-
 let grid_width = 0;
 let grid_height = 0;
 
-let boid_x = 0;
-let boid_y = 0;
-
-let vel_x = 0;
-let vel_y = 0;
-
-let target_x = 0;
-let target_y = 0;
-
-function preload() {
-  dina = loadFont("assets/dina.ttf");
-  dina_bold = loadFont("assets/dina_bold.ttf");
-}
+let cells = [];
 
 function setup() {
   grid_width = floor(windowWidth / 32);
   grid_height = floor(windowHeight / 32);
 
-  for (let i = 0; i < grid_height; i++) {
-    let row = [];
+  for (let i = 0; i < 1000; i++) {
+    for (let tries = 0; tries < 100; tries++) {
+      let pos = [random(0, grid_width), random(0, grid_height)];
+      let valid = true;
 
-    for (let j = 0; j < grid_width; j++) {
-      row.push(0);
+      for (let cell of cells) {
+        if ((cell[0] - pos[0]) * (cell[0] - pos[0]) + (cell[1] - pos[1]) * (cell[1] - pos[1]) < 16) {
+          valid = false;
+        }
+      }
+
+      if (valid) {
+        cells.push(pos);
+        break;
+      }
     }
-
-    grid.push(row);
   }
 
-  // noSmooth();
+  noSmooth();
   createCanvas(windowWidth, windowHeight);
-
-  boid_x = grid_width / 2;
-  boid_y = grid_height / 2;
-
-
 }
 
 function draw() {
-  background(0, 0, 0);
+  background(0);
   noStroke();
-
-  let mx = mouseX / 32;
-  let my = mouseY / 32;
-
-  if (mx < 0) mx = 0;
-  if (my < 0) my = 0;
-
-  if (mx >= grid_width - 0.001) mx = grid_width - 0.001;
-  if (my >= grid_height - 0.001) my = grid_height - 0.001;
 
   for (let i = 0; i < grid_height; i++) {
     for (let j = 0; j < grid_width; j++) {
-      let value = grid[i][j];
+      let pos = [j, i];
 
-      let dist_1 = (my - i) * (my - i) + (mx - j) * (mx - j) + 30;
-      let dist_2 = (boid_y - i) * (boid_y - i) + (boid_x - j) * (boid_x - j);
+      let best_dist = 1000000;
+      let best_cell;
 
-      let dist = dist_1 < dist_2 ? dist_1 : dist_2;
+      for (let cell of cells) {
+        let dist = (cell[0] - pos[0]) * (cell[0] - pos[0]) + (cell[1] - pos[1]) * (cell[1] - pos[1]);
 
-      fill(map(value, 0.5, 1, 0, 127), map(value, 0.5, 1, 0, 127), map(value, 0, 0.75, 0, 255));
+        if (dist < best_dist) {
+          best_cell = cell;
+          best_dist = dist;
+        }
+      }
+
+      pos = [mouseX / 32, mouseY / 32];
+      let dist = (best_cell[0] - pos[0]) * (best_cell[0] - pos[0]) + (best_cell[1] - pos[1]) * (best_cell[1] - pos[1]);
+
+      let value = sin(noise(best_cell[0] / 10, best_cell[1] / 10, millis() / 4000)) * 64;
+      if (value < 0) value = 0;
+
+      if (dist < 128) {
+        value += map(dist, 0, 128, 63, 0);
+      }
+
+      fill(0, 0, value);
       rect(j * 32, i * 32, 32, 32);
-
-      grid[i][j] *= 0.98;
-
-      if (dist < 36) {
-        grid[i][j] += map(dist, 0, 36, 0.05, 0);
-      }
     }
-  }
-
-  let dist = sqrt((target_x - boid_x) * (target_x - boid_x) + (target_y - boid_y) * (target_y - boid_y));
-
-  vel_x += ((target_x - boid_x) / dist) * 0.05;
-  vel_y += ((target_y - boid_y) / dist) * 0.05;
-
-  boid_x += vel_x;
-  boid_y += vel_y;
-
-  vel_x *= 0.98;
-  vel_y *= 0.98;
-
-  if (dist < 1) {
-    for (let i = 0; i < 10; i++) {
-      let x = random(4, grid_width - 4);
-      let y = random(4, grid_height - 4);
-
-      let temp_dist = sqrt((x - boid_x) * (x - boid_x) + (y - boid_y) * (y - boid_y));
-
-      if (temp_dist > dist) {
-        target_x = x;
-        target_y = y;
-        dist = temp_dist;
-      }
-    }
-  }
-
-  if (boid_x < -12) {
-    boid_x = -12;
-    vel_x = 0;
-  }
-
-  if (boid_y < -12) {
-    boid_y = -12;
-    vel_y = 0;
-  }
-
-  if (boid_x > grid_width + 12) {
-    boid_x = grid_width + 12;
-    vel_x = 0;
-  }
-
-  if (boid_y > grid_height + 12) {
-    boid_y = grid_height + 12;
-    vel_y = 0;
   }
 }
